e4feb4c621d490fa32bd0dae4744d4b4b9e59867
bosiTomi <bosanszkitamas@gmail.com>
2022-09-15 16:54:24 +0200

    commented unused code for now

--- Controllers/PotionController.cs
+++ Controllers/PotionController.cs
@@ -1,61 +1,61 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using HogwartsPotions.Models;
-using HogwartsPotions.Models.Entities;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using HogwartsPotions.Models;
+//using HogwartsPotions.Models.Entities;
+//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc;
 
-namespace HogwartsPotions.Controllers
-{
+//namespace HogwartsPotions.Controllers
+//{
     
-    public class PotionController : Controller
-    {
-        private readonly HogwartsContext _context;
-        public PotionController(HogwartsContext context)
-        {
-            _context = context;
-        }
+//    public class PotionController : Controller
+//    {
+//        private readonly HogwartsContext _context;
+//        public PotionController(HogwartsContext context)
+//        {
+//            _context = context;
+//        }
 
-        public IActionResult Index()
-        {
-            return View(_context.Potions.ToList());
-        }
+//        public IActionResult Index()
+//        {
+//            return View(_context.Potions.ToList());
+//        }
 
-        [HttpGet]
-        public async Task<List<Potion>> GetPotions()
-        {
-            return await _context.GetPotions();
-        }
+//        [HttpGet]
+//        public async Task<List<Potion>> GetPotions()
+//        {
+//            return await _context.GetPotions();
+//        }
 
-        [HttpPost]
-        public async Task<Potion> CreatePotion([FromBody]Potion potion)
-        {
-            return await _context.CreateAPotion(potion);
-        }
+//        [HttpPost]
+//        public async Task<Potion> CreatePotion([FromBody]Potion potion)
+//        {
+//            return await _context.CreateAPotion(potion);
+//        }
 
-        [HttpGet("/potions/{studentId:long}")]
-        public async Task<List<Potion>> GetAllPotionsOfAStudent(long studentId)
-        {
-            return await _context.GetPotionsOfAStudent(await _context.GetStudent(studentId));
-        }
+//        [HttpGet("/potions/{studentId:long}")]
+//        public async Task<List<Potion>> GetAllPotionsOfAStudent(long studentId)
+//        {
+//            return await _context.GetPotionsOfAStudent(await _context.GetStudent(studentId));
+//        }
 
-        [HttpPost("/potions/brew")]
-        public async Task<Potion> FinishPotion([FromBody] Potion potion)
-        {
-            return await _context.FinishPotion(potion);
-        }
+//        [HttpPost("/potions/brew")]
+//        public async Task<Potion> FinishPotion([FromBody] Potion potion)
+//        {
+//            return await _context.FinishPotion(potion);
+//        }
 
-        [HttpPut("/potions/{potionId}/add")]
-        public async Task<Potion> AddIngredient([FromBody] Ingredient ingredient, long potionId)
-        {
-            return await _context.AddIngredient(ingredient, potionId);
-        }
+//        [HttpPut("/potions/{potionId}/add")]
+//        public async Task<Potion> AddIngredient([FromBody] Ingredient ingredient, long potionId)
+//        {
+//            return await _context.AddIngredient(ingredient, potionId);
+//        }
 
-        [HttpGet("/potions/{potionId}/help")]
-        public async Task<List<Recipe>> GetRecipes(long potionId)
-        {
-            return await _context.GetRecipes(potionId);
-        }
-    }
-}
+//        [HttpGet("/potions/{potionId}/help")]
+//        public async Task<List<Recipe>> GetRecipes(long potionId)
+//        {
+//            return await _context.GetRecipes(potionId);
+//        }
+//    }
+//}
