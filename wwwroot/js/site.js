13e7e0b70738910298e9f17bf57bc884769f06b7
bosiTomi <bosanszkitamas@gmail.com>
2022-10-18 15:39:59 +0200

    added ingredients hide/show, not enough ingredients selected alert, and searchbar script

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -10,6 +10,46 @@ let registerButton = document.querySelector('#register');
 let loginForm = document.querySelector('#login-form');
 let registerForm = document.querySelector('#register-form');
 let warning = document.querySelector('#warning');
+
+function showIngredients() {
+    let ingredientsList = document.querySelector('#ingredients');
+    let backToClassButton = document.querySelector('.back-to-class');
+    let createButton = document.querySelector('#create');
+    if (ingredientsList.style.display === "none") {
+        ingredientsList.style.display = "block";
+        backToClassButton.style.display = "none";
+        createButton.style.display = "none";
+        document.getElementById("createDiv").style.zIndex = -999;
+    } else {
+        ingredientsList.style.display = "none";
+        backToClassButton.style.display = "block";
+        createButton.style.display = "block";
+        document.getElementById("createDiv").style.zIndex = 999;
+    }
+}
+
+function Alert() {
+    if (document.getElementById("create").hasAttribute("disabled")) {
+        alert("Select 5 ingredients");
+    }
+};
+
+function SearchIngredient() {
+    var ingredients = document.getElementsByTagName("option");
+    var searchText = document.getElementById('searchBar').value.toLowerCase();
+
+    for (var i = 0; i < ingredients.length; i++) {
+        if (!ingredients[i].value.toLowerCase().includes(searchText)) {
+            ingredients[i].style.display = "none";
+        }
+        else {
+            ingredients[i].style.display = "block";
+        };
+    }
+    return false;
+};
+
+
 loginButton.addEventListener('click', () => {
     registerForm.setAttribute('hidden', 'hidden');
     loginForm.removeAttribute('hidden');
@@ -20,3 +60,4 @@ registerButton.addEventListener('click', () => {
     registerForm.removeAttribute('hidden');
     warning.innerHTML = "";
 });
+
