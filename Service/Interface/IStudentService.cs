24ade9c98220088993b58c3e44217bf23cb26f08
Tamás Bosánszki <bosanszkitamas@gmail.com>
2023-01-05 15:24:09 +0100

    added Interface for Student Service

---
+++ Service/Interface/IStudentService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using HogwartsPotions.Models;
+using HogwartsPotions.Models.Entities;
+
+namespace HogwartsPotions.Service.Interface;
+
+public interface IStudentService
+{
+    Task AddStudent(Student student);
+    Task<Student> GetStudent(long studentId);
+    Task<Student> GetStudentByUsername(string username);
+    Task<List<Student>> GetAllStudents();
+    Task UpdateStudent(Student student);
+    Task DeleteStudent(long id);
+    bool ValidateLogin(LoginForm user);
+    bool Register(Student user);
+}
